1410cfa9d2c3cd430d45e37705b51bdc1b922d64
shijingyu <945090896@qq.com>
2017-10-28 09:52:14 +0800

    Initial commit

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 17-10-28 上午9:40
+# @Author  : shitouBoy
+# @Email   : xy960722@gmail.com
+# @File    : serializers.py
+# @Describe:
+# @Software: PyCharm
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        madel = User
+        field = ('url', 'username', 'email', 'groups')
+
+    class GroupSerializer(serializers.HyperlinkedModelSerializer):
+        class Meta:
+            model = User
+            fields = ('url', 'name')
