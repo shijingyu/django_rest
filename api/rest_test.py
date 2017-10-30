9be64461809cd11a01fa34414bee47c5ef6b180c
shijingyu <945090896@qq.com>
2017-10-30 22:01:35 +0800

    restful api(users,groups)

---
+++ api/rest_test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 17-10-30 下午10:00
+# @Author  : shitouBoy
+# @Email   : xy960722@gmail.com
+# @File    : rest_test.py
+# @Describe:
+# @Software: PyCharm
+
+import unittest
+
--- api/views.py
+++ api/views.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from api.serializers import UserSerializer, GroupSerializer
+
+#ViewSets　定义视图的展现形式
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
